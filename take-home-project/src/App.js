de01f9bdd4b51ac7efaf06a7fa48fdbbff371e47
Forrest-Darabian <mr.smiley72@yahoo.com>
2021-06-29 14:26:58 -0700

    Set up Actions for Redux

--- take-home-project/src/App.js
+++ take-home-project/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-
+import { getServices } from './actions';
 function App(props) {
   return (
     <div className="App">
       <h1>Take-Home Project</h1>
+      {props.loading && <div>loading...</div>}
+      {/* If services are available, display a div */}
+      {props.services && (<div>
+        <p>{props.services.id}</p>
+      </div>)}
+      {/* {props.error !== "" && <p>{error}</p>} */}
+      <button onClick={props.getServices}>View all Services</button>
     </div>
   );
 }
@@ -19,4 +26,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null) (App);
+export default connect(mapStateToProps, { getServices })(App);
