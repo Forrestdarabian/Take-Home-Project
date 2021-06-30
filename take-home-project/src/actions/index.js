ac00e9283df705c56bd3874596fe5bd7489fe47f
Forrest-Darabian <mr.smiley72@yahoo.com>
2021-06-29 18:11:17 -0700

    API endpoint blocked by CORS, making dummy JSON

--- take-home-project/src/actions/index.js
+++ take-home-project/src/actions/index.js
@@ -1,18 +1,33 @@
 // Actions are dispatched messages that pass through reducers
 
 import axios from 'axios';
+import data from "../data.json"
 export const FETCH_SERVICES_START = "FETCH_SERVICES_START";
 export const FETCH_SERVICES_SUCCESS = "FETCH_SERVICES_SUCCESS";
 export const FETCH_SERVICES_FAILURE = "FETCH_SERVICES_FAILURE";
 
 export const getServices = () => dispatch => {
     dispatch({ type: FETCH_SERVICES_START });
-    axios.get("https://johnny-appleseed.clientsecure.me/client-portal-api/cpt-codes?filter[clinicianId]=2", 
-    { 'headers': { "Accept": "application/vnd.api+json", "Api-Version": "2020-01-01", "Application-Build-Version": "0.0.1", "Application-Platform": "web"
-         } })
-    .then(res => dispatch({ type: FETCH_SERVICES_SUCCESS, payload: res.data}))
-    .catch(error => {
-        console.log(error, error.response);
-        dispatch({ type: FETCH_SERVICES_FAILURE, payload: error.response });
-    });
+
+if(data) {
+    dispatch({ type: FETCH_SERVICES_SUCCESS, payload: data });
+} else{
+    dispatch({ type: FETCH_SERVICES_FAILURE, payload: "error" });
+
+}
+
+//     axios.get('https://johnny-appleseed.clientsecure.me/client-portal-api/cpt-codes?filter[clinicianId]=2', {
+//   headers:{
+//     'Accept':' application/vnd.api+json',
+// 'Api-Version':' 2020-01-01',
+// 'Application-Build-Version':' 0.0.1',
+// 'Application-Platform': 'web',
+//   }
+// })
+// .then(res => console.log(res.data))
+
+//     .catch(error => {
+//         console.log(error, error.response);
+//         dispatch({ type: FETCH_SERVICES_FAILURE, payload: error.response });
+//     });
 };
