d67c18f8900c4fa925e9e57cef8569c0a6612838
Forrest-Darabian <mr.smiley72@yahoo.com>
2021-06-29 18:48:12 -0700

    Dummy data request is working for first page

--- take-home-project/src/App.js
+++ take-home-project/src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { getServices } from './actions';
 function App(props) {
+  const dispatch = useDispatch();
+  useEffect(() => {dispatch(getServices())}, [])
+  console.log(props);
   return (
     <div className="App">
       <h1>Take-Home Project</h1>
       {props.loading && <div>loading...</div>}
       {/* If services are available, display a div */}
-      {props.services && (<div>
-        <p>{props.services.id}</p>
+      {props.services && 
+      props.services.data && 
+      (<div>
+        {props.services.data.map((index, key) => {
+          return (<div key={key}>
+          <p>{index.id}</p>
+          <p>{index.attributes.description}</p>
+          <p>{index.attributes.duration}</p>
+          <p>{index.attributes.rate}</p>
+          <p>{index.attributes.callToBook}</p>
+        </div>)}) }
       </div>)}
       {props.error !== "" && <p>{props.error}</p>}
-      <button onClick={props.getServices}>View all Services</button>
+      {/* <button onClick={props.getServices}>View all Services</button> */}
     </div>
   );
 }
 
 const mapStateToProps = state => {
+  console.log(state, "this is our state")
   return {
     services: state.services,
     error: state.error,
